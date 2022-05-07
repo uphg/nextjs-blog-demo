79a3a98da03184ca65db2b37204dc9711598f8ab
chen <1834805770@qq.com>
2022-05-07 22:17:15 +0800

    添加 .env.local

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -4,8 +4,7 @@ import { NextApiHandler } from 'next'
 import { User } from 'src/entity/User'
 
 export const sessionOptions: IronSessionOptions = {
-  // password: process.env.SECRET_COOKIE_PASSWORD as string,
-  password: '4821e0cd-9ad5-468a-a0f9-437a4d29ef1e',
+  password: process.env.SECRET as string,
   cookieName: 'blog',
   // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
   cookieOptions: {
