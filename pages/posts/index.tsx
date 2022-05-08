8e0c07dcb5488607bae459a74ea933664fd187d0
chen <1834805770@qq.com>
2022-05-08 21:24:59 +0800

    实现获取分页的博客数据

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps, NextPage } from 'next'
-import { Post } from 'src/entity/Post'
 import { UAParser } from 'ua-parser-js'
+import qs from 'query-string'
+import { Post } from 'src/entity/Post'
 import { getDataSource } from 'lib/getDataSource'
 import Link from "next/link"
 
@@ -10,7 +11,10 @@ interface Props {
     version: string;
     major: string;
   },
-  posts: Post[]
+  posts: Post[];
+  count: number;
+  page: number;
+  total: number;
 }
 
 const PostsIndex: NextPage<Props> = (props) => {
@@ -21,13 +25,36 @@ const PostsIndex: NextPage<Props> = (props) => {
       {props.posts.map((item) => <div key={item.id}>
         <Link href={`/posts/${item.id}`}><a>{item.title}</a></Link>
       </div>)}
+      <div>
+        <div>共 {props.count} 篇文章，当前 {props.page}/{props.total}</div>
+        <div>
+          {props.page > 1 && <Link href={`?page=${props.page - 1}`}>
+            <a>上一页</a>
+          </Link>}
+          {props.page < props.total && <Link href={`?page=${props.page + 1}`}>
+            <a>下一页</a>
+          </Link>}
+        </div>
+      </div>
     </div>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  // const posts = await myDataSource.manager.find(Post)
+  
+  // 获取分页
   const myDataSource = await getDataSource()
-  const posts = await myDataSource.manager.find(Post)
+  const perPage = 3
+  const url = context.req.url
+  const index = url.indexOf('?')
+  const search = url.substring(index + 1)
+  const query = qs.parse(search)
+  const page = parseInt(query.page?.toString()) || 1
+  const [posts, count] = await myDataSource.manager.findAndCount(Post, {
+    skip: (page - 1) * perPage,
+    take: perPage
+  })
 
   const ua = context.req.headers['user-agent']
   const result = new UAParser(ua).getResult()
@@ -35,7 +62,10 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     props: {
       browser: result.browser,
-      posts: JSON.parse(JSON.stringify(posts))
+      posts: JSON.parse(JSON.stringify(posts)),
+      count,
+      page,
+      total: Math.ceil(count / perPage)
     }
   }
 }
