bdc4414b0cc41ab1128b8bedc8fbd730b57a6d54
chen <1834805770@qq.com>
2022-05-08 15:30:02 +0800

    实现创建博客 api 接口

--- pages/api/v1/posts.tsx
+++ pages/api/v1/posts.tsx
@@ -1,13 +1,20 @@
-import { getPosts } from "lib/posts"
+import { getDataSource } from "lib/getDataSource"
+import { withSessionRoute } from "lib/withSession"
 import { NextApiHandler } from "next"
+import { Post } from "src/entity/Post"
 
-const Posts: NextApiHandler = async (request, response) => {
-  response.statusCode = 200
-  response.setHeader('Content-Type', 'application/json')
-  // const posts = await getPosts()
-  // response.write(JSON.stringify(posts))
-  response.write(JSON.stringify({ content: '内容' }))
-  response.end()
-}
+const Posts: NextApiHandler = withSessionRoute(async (req, res) => {
+  if (req.method === 'POST') {
+    const { title, content } = req.body
+    const post = new Post()
+    post.title = title
+    post.content = content
+    const user = req.session.currentUser
+    post.author = user
+    const myAppDataSource = await getDataSource()
+    await myAppDataSource.manager.save(post)
+    res.json(post)
+  }
+})
 
 export default Posts
