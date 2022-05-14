1d1050a6b458b2142323d56593c1b29dc692229e
chen <1834805770@qq.com>
2022-05-14 21:12:34 +0800

    优化 getDataSource 方法

--- lib/getDataSource.tsx
+++ lib/getDataSource.tsx
@@ -1,13 +1,18 @@
 import { AppDataSource } from "src/data-source"
 
-const promise = (async function () {
+// const promise = (async function () {
+//   if (AppDataSource.isInitialized) {
+//     return AppDataSource
+//   } else {
+//     return AppDataSource.initialize()
+//   }
+// })();
+
+export const getDataSource = async () => {
   if (AppDataSource.isInitialized) {
     return AppDataSource
   } else {
     return AppDataSource.initialize()
   }
-})();
-
-export const getDataSource = async () => {
-  return promise
+  // return promise
 }
